576cebfbc0f9cba42c3546dfaf0c6c509dafa2d9
Student <Student@SZN-SILS0258>
2024-05-23 09:37:03 +0200

    Dodaj pliki projektów.

---
+++ KlasaZKonstruktorem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Konstruktory
+{
+    class KlasaZKonstruktorem
+    {
+        private KlasaZKonstruktorem()
+        {
+            Console.WriteLine("Nic nie podano!");
+        }
+        private KlasaZKonstruktorem(string tekstDoKonsoli)
+        {
+            Console.WriteLine(tekstDoKonsoli);
+        }
+
+        public static KlasaZKonstruktorem Metoda()
+        {
+            return new KlasaZKonstruktorem();
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Konstruktory;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            KlasaZKonstruktorem klasa;
+            klasa = KlasaZKonstruktorem.Metoda();
+        }
+    }
+
+}
+
+
+
